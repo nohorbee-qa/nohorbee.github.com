79ca08e882e22d7305f9418178bdd45765201c20
Norberto L. Herz <nohorbee@Norbertos-MacBook-Pro.local>
2015-02-27 22:43:34 -0300

    Sections created

--- js/effects.js
+++ js/effects.js
@@ -27,3 +27,19 @@ $(document).scroll(function() {
         }
     }
 });
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        var offset = (target.selector == '#profile' ? 100 : 70);
+        $('html,body').animate({
+          scrollTop: target.offset().top-offset
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
